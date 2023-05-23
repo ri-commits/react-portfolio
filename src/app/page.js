75216b160b6da540cbb88846ce873a5717ec586d
ri-commits <kartashourychard@gmail.com>
2023-05-23 22:58:48 +0300

    Finish

--- src/app/page.js
+++ src/app/page.js
@@ -1,3 +1,4 @@
+"use client";
 import { BsFillMoonStarsFill } from "react-icons/bs";
 import {
   AiFillTwitterCircle,
@@ -9,113 +10,174 @@ import deved from "../../public/dev-ed-wave.png";
 import design from "../../public/design.png";
 import code from "../../public/code.png";
 import consulting from "../../public/consulting.png";
+import web1 from "../../public/web1.png";
+import web2 from "../../public/web2.png";
+import web3 from "../../public/web3.png";
+import web4 from "../../public/web4.png";
+import web5 from "../../public/web5.png";
+import web6 from "../../public/web6.png";
+import { useState } from "react";
+
+const portfolioImg = [web1, web2, web3, web4, web5, web6];
 
 export default function Home() {
+  const [darkMode, setDarkMode] = useState(false);
   return (
-    <main className="bg-white px-10">
-      <section className=" min-h-screen">
-        <nav className="py-10 mb-12 flex justify-between">
-          <h1 className=" text-xl font-burtons">developbyry</h1>
-          <ul className="flex items-center">
-            <li>
-              <BsFillMoonStarsFill className="cursor-pointer text-2xl" />
-            </li>
-            <li>
-              <a
-                className="bg-gradient-to-r from-cyan-500 to-teal-500 text-white px-4 py-2 rounded-md ml-8"
-                href="#"
-              >
-                Resume
-              </a>
-            </li>
-          </ul>
-        </nav>
+    <div className={darkMode ? "dark" : ""}>
+      <main className="bg-white px-10 md:px-20 lg:px-40 dark:bg-gray-900">
+        <section className=" min-h-screen">
+          <nav className="py-10 mb-12 flex justify-between">
+            <h1 className=" text-xl font-burtons dark:text-white">
+              developbyry
+            </h1>
+            <ul className="flex items-center">
+              <li>
+                <BsFillMoonStarsFill
+                  onClick={() => setDarkMode(!darkMode)}
+                  className="cursor-pointer text-2xl dark:text-white"
+                />
+              </li>
+              <li>
+                <a
+                  className="bg-gradient-to-r from-cyan-500 to-teal-500 text-white px-4 py-2 rounded-md ml-8"
+                  href="#"
+                >
+                  Resume
+                </a>
+              </li>
+            </ul>
+          </nav>
 
-        <div className="text-center p-10">
-          <h2 className="text-5xl py-2 text-teal-600 font-medium">
-            Richard Kartashou
-          </h2>
-          <h3 className="text-2xl py-2">Developer and designer.</h3>
-          <p className="text-md py-5 leading-8 text-gray-800 ">
-            Are you in search of top-notch programming and design services for
-            your project? Look no further! I'm a skilled freelance developer and
-            designer, ready to bring your vision to life. Join me on this
-            exciting journey, and let's get cracking!
-          </p>
-        </div>
-
-        <div className="text-5xl flex justify-center gap-16 py-3 text-gray-600">
-          <AiFillTwitterCircle />
-          <AiFillLinkedin />
-          <AiFillYoutube />
-        </div>
-        <div className="relative mx-auto bg-gradient-to-b from-teal-500 rounded-full w-80 h-80 mt-20 overflow-hidden">
-          <Image src={deved} fill objectFit="cover" />
-        </div>
-      </section>
-
-      {/* Second Page */}
-
-      <section>
-        <div>
-          <h3 className="text-3xl py-1">Services I offer</h3>
-          <p className="text-md py-2 leading-8 text-gray-800">
-            Since the beginning of my journey as freelancer developer, I've done
-            remote work for
-            <span className="text-teal-500"> agences </span>
-            consulted for <span className="text-teal-500"> startaps </span> and
-            collaborated with talanted people to create digital products for
-            both buisness and consumer use.
-          </p>
-          <p className="text-md py-2 leading-8 text-gray-800 dark:text-gray-200">
-            I offer from a wide range of services, including brand design,
-            programming and teaching.
-          </p>
-        </div>
-        <div>
-          <div className="  text-center shadow-lg p-10 rounded-xl my-10">
-            <div className="flex justify-center">
-              <Image src={design} width={100} height={100} />
-            </div>
-            <h3 className=" text-lg font-medium pt-8 pb-2">Beautiful Design</h3>
-            <p className="py-2">
-              Creating elegant designs suited for your needs design theory
+          <div className="text-center p-10">
+            <h2 className="text-5xl py-2 text-teal-600 font-medium md:text-6xl">
+              Richard Kartashou
+            </h2>
+            <h3 className="text-2xl py-2 md:text-3xl dark:text-white">
+              Developer and designer.
+            </h3>
+            <p className="text-md py-5 leading-8 text-gray-800 md:text-xl max-w-xl mx-auto dark:text-gray-200">
+              Are you in search of top-notch programming and design services for
+              your project? Look no further! I'm a skilled freelance developer
+              and designer, ready to bring your vision to life. Join me on this
+              exciting journey, and let's get cracking!
             </p>
-            <h4 className="py-4 text-teal-600">Design tools I use</h4>
-            <p className="text-gray-800 py-1">Photoshop</p>
-            <p className="text-gray-800 py-1">Illustrator</p>
-            <p className="text-gray-800 py-1">Figma</p>
           </div>
 
-          <div className=" text-center shadow-lg p-10 rounded-xl my-10">
-            <div className="flex justify-center">
-              <Image src={consulting} width={100} height={100} />
-            </div>
-            <h3 className=" text-lg font-medium pt-8 pb-2">Beautiful Design</h3>
-            <p className="py-2">
-              Creating elegant designs suited for your needs design theory
-            </p>
-            <h4 className="py-4 text-teal-600">Design tools I use</h4>
-            <p className="text-gray-800 py-1">Photoshop</p>
-            <p className="text-gray-800 py-1">Illustrator</p>
-            <p className="text-gray-800 py-1">Figma</p>
+          <div className="text-5xl flex justify-center gap-16 py-3 text-gray-600">
+            <AiFillTwitterCircle />
+            <AiFillLinkedin />
+            <AiFillYoutube />
           </div>
+          <div className="relative mx-auto bg-gradient-to-b from-teal-500 rounded-full w-80 h-80 mt-20 overflow-hidden md:h-96 md:w-96">
+            <Image src={deved} fill objectFit="cover" />
+          </div>
+        </section>
 
-          <div className=" text-center shadow-lg p-10 rounded-xl my-10">
-            <div className="flex justify-center">
-              <Image src={code} width={100} height={100} />
-            </div>
-            <h3 className=" text-lg font-medium pt-8 pb-2">Beautiful Design</h3>
-            <p className="py-2">
-              Creating elegant designs suited for your needs design theory
+        {/* Second Page */}
+
+        <section>
+          <div>
+            <h3 className="text-3xl py-1 dark:text-white">Services I offer</h3>
+            <p className="text-md py-2 leading-8 text-gray-800 dark:text-gray-200">
+              Since the beginning of my journey as freelancer developer, I've
+              done remote work for
+              <span className="text-teal-500"> agences </span>
+              consulted for <span className="text-teal-500">
+                {" "}
+                startaps{" "}
+              </span>{" "}
+              and collaborated with talanted people to create digital products
+              for both buisness and consumer use.
             </p>
-            <h4 className="py-4 text-teal-600">Design tools I use</h4>
-            <p className="text-gray-800 py-1">Photoshop</p>
-            <p className="text-gray-800 py-1">Illustrator</p>
-            <p className="text-gray-800 py-1">Figma</p>
+            <p className="text-md py-2 leading-8 text-gray-800 dark:text-gray-200 ">
+              I offer from a wide range of services, including brand design,
+              programming and teaching.
+            </p>
           </div>
-        </div>
-      </section>
-    </main>
+          <div className="lg:flex gap-10 ">
+            <div className="  text-center shadow-lg p-10 rounded-xl my-10 dark:bg-gray-200">
+              <div className="flex justify-center">
+                <Image src={design} width={100} height={100} />
+              </div>
+              <h3 className=" text-lg font-medium pt-8 pb-2">
+                Beautiful Design
+              </h3>
+              <p className="py-2">
+                Creating elegant designs suited for your needs design theory
+              </p>
+              <h4 className="py-4 text-teal-600">Design tools I use</h4>
+              <p className="text-gray-800 py-1">Photoshop</p>
+              <p className="text-gray-800 py-1">Illustrator</p>
+              <p className="text-gray-800 py-1">Figma</p>
+            </div>
+
+            <div className=" text-center shadow-lg p-10 rounded-xl my-10 dark:bg-gray-200">
+              <div className="flex justify-center">
+                <Image src={consulting} width={100} height={100} />
+              </div>
+              <h3 className=" text-lg font-medium pt-8 pb-2">
+                Beautiful Design
+              </h3>
+              <p className="py-2">
+                Creating elegant designs suited for your needs design theory
+              </p>
+              <h4 className="py-4 text-teal-600">Design tools I use</h4>
+              <p className="text-gray-800 py-1">Photoshop</p>
+              <p className="text-gray-800 py-1">Illustrator</p>
+              <p className="text-gray-800 py-1">Figma</p>
+            </div>
+
+            <div className=" text-center shadow-lg p-10 rounded-xl my-10 dark:bg-gray-200">
+              <div className="flex justify-center">
+                <Image src={code} width={100} height={100} />
+              </div>
+              <h3 className=" text-lg font-medium pt-8 pb-2">
+                Beautiful Design
+              </h3>
+              <p className="py-2">
+                Creating elegant designs suited for your needs design theory
+              </p>
+              <h4 className="py-4 text-teal-600">Design tools I use</h4>
+              <p className="text-gray-800 py-1">Photoshop</p>
+              <p className="text-gray-800 py-1">Illustrator</p>
+              <p className="text-gray-800 py-1">Figma</p>
+            </div>
+          </div>
+        </section>
+        <section>
+          <div>
+            <h3 className="text-3xl py-1 dark:text-white">Portofolio</h3>
+            <p className="text-md py-2 leading-8 text-gray-800 dark:text-gray-200">
+              Since the beginning of my journey as freelancer developer, I've
+              done remote work for
+              <span className="text-teal-500"> agences </span>
+              consulted for <span className="text-teal-500">
+                {" "}
+                startaps{" "}
+              </span>{" "}
+              and collaborated with talanted people to create digital products
+              for both buisness and consumer use.
+            </p>
+            <p className="text-md py-2 leading-8 text-gray-800 dark:text-gray-200 ">
+              I offer from a wide range of services, including brand design,
+              programming and teaching.
+            </p>
+          </div>
+          <div className="flex flex-col gap-10 py-10 lg:flex-row lg:flex-wrap">
+            {portfolioImg.map((image, index) => (
+              <div className="basis-1/3 flex-1">
+                <Image
+                  src={image}
+                  key={index}
+                  className="rounded-lg object-cover"
+                  width={"100%"}
+                  height={"100%"}
+                />
+              </div>
+            ))}
+          </div>
+        </section>
+      </main>
+    </div>
   );
 }
